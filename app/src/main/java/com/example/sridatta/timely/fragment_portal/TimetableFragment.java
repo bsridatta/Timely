8266104770a6c9ca34135345c665a20dc344512a
bsridatta <b.sridatta@gmail.com>
2017-11-28 22:51:44 +0530

    second stable basic updated dependacies

---
+++ app/src/main/java/com/example/sridatta/timely/fragment_portal/TimetableFragment.java
@@ -0,0 +1,32 @@
+package com.example.sridatta.timely.fragment_portal;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.sridatta.timely.R;
+
+public class TimetableFragment extends Fragment {
+
+    public TimetableFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_timetable, container, false);
+    }
+
+}
